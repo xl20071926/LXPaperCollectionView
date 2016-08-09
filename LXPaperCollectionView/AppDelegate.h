f3844fe03c50307494b3afc9d9f1f810ea901730
xl20071926 <lixin_1990@yeah.net>
2016-08-09 10:20:40 +0800

    Initial Commit

---
+++ LXPaperCollectionView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LXPaperCollectionView
+//
+//  Created by Leexin on 16/8/9.
+//  Copyright © 2016年 Garden.Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LXPaperCollectionView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LXPaperCollectionView
+//
+//  Created by Leexin on 16/8/9.
+//  Copyright © 2016年 Garden.Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
