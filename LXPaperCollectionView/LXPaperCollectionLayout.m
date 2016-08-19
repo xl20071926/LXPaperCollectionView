02bf06e3d380447bca4af58df1c305136fb97b1a
xl20071926 <lixin_1990@yeah.net>
2016-08-19 11:45:49 +0800

    First commit

---
+++ LXPaperCollectionView/LXPaperCollectionLayout.m
@@ -0,0 +1,13 @@
+//
+//  LXPaperCollectionLayout.m
+//  LXPaperCollectionView
+//
+//  Created by Leexin on 16/8/9.
+//  Copyright © 2016年 Garden.Lee. All rights reserved.
+//
+
+#import "LXPaperCollectionLayout.h"
+
+@implementation LXPaperCollectionLayout
+
+@end
